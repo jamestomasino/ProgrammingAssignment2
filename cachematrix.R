cff3a5bb5eb7c5905743eb026630608841c3aa58
James Tomasino <james@tomasino.org>
2014-05-24 21:47:08 -0400

    setting up basic function comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix
+##   creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve
+##   Calculates the inverse of a matrix, or gets cache if it exists
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
